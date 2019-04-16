052ecbf2e1ad29777d396cb2aadcebaf802c32e6
Andrew Campbell <andycampbell92@gmail.com>
2019-04-16 21:39:23 +0100

    checks the sequence and sets output pin high for 5 seconds on success

--- arduino_button_sketch/arduino_button_sketch.ino
+++ arduino_button_sketch/arduino_button_sketch.ino
@@ -1,8 +1,10 @@
 int buttonPins[] = {2, 3, 4, 5};
-int buttonPinsCount = 4;
+int buttonPinsCount = sizeof(buttonPins) / sizeof(int);
 const int outputPin =  13;
 int sequence[]= {3, 5, 2, 4};
+int sequenceCount = sizeof(sequence) / sizeof(int);
 int correctGuesses = 0;
+int lastInputPin = -1;
 
 void setup() {
   for(int i=0; i < buttonPinsCount; i++){
@@ -12,18 +14,25 @@ void setup() {
 }
 
 void loop() {
-  bool inputRegistered = false;
   for(int i=0; i < buttonPinsCount; i++){
     int pin = buttonPins[i];
     int pinState = digitalRead(pin);
-    if (pinState == HIGH){
-      digitalWrite(outputPin, HIGH);
-      inputRegistered = true;
+    // Check if pin HIGH and that it's not the same pin we got last time (they are holding the button)
+    // This means the sequence won't reset if someone tries the same button twice
+    if (pinState == HIGH && pin != lastInputPin){
+      lastInputPin = pin;
+      if (pin == sequence[correctGuesses]){
+        correctGuesses++;
+      } else{
+        correctGuesses = 0;
+      }
       break;
     }
   }
-  
-  if(!inputRegistered){
+  if (correctGuesses == sequenceCount){
+    correctGuesses = 0;
+    digitalWrite(outputPin, HIGH);
+    delay(5000);
     digitalWrite(outputPin, LOW);
   }
 }
